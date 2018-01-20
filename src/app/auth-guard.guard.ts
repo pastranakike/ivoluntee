b5e5b6ff2a140f6aa8261fae4b55b44e96c529f5
pastranakike <lepl3@gatech.edu>
2018-01-20 11:26:41 -0500

    Login Fixed

--- src/app/auth-guard.guard.ts
+++ src/app/auth-guard.guard.ts
@@ -3,16 +3,31 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angul
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class AuthGuardGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
 
   canActivate( next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    var subject = new Subject<boolean>();
     const logged = this.auth.isLoggedIn();
-    if (!logged) {
-      this.router.navigate(['/']);
-    }
-    return logged;
+    let count = 0;
+    let loggedIn = false;
+    const interval = window.setInterval(() => {
+      if (this.auth.isLoggedIn()) {
+        loggedIn = true;
+        console.log(loggedIn);
+      }
+      count++;
+      if (count >= 4 || loggedIn) {
+          if (!loggedIn) {
+            this.router.navigate(['/']);
+          }
+          subject.next(loggedIn);
+          window.clearInterval(interval);
+      }
+    }, 200);
+    return subject.asObservable();
   }
 }
